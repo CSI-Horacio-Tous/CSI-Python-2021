0d30722bf9b8788fd475e4c6db3396bb3e4e78e2
CSI-Horacio-Tous <89258670+CSI-Horacio-Tous@users.noreply.github.com>
2021-08-30 09:13:21 -0400

    Create ACSII-initials.py
    ASCII Tous

---
+++ Projects/ACSII-initials.py
@@ -0,0 +1,11 @@
+print("""
+
+TTTTTTT OOOOOOOO  U       U SSSSSSSSS
+  |T|   O      O  U       U S
+  |T|   O      O  U       U S
+  |T|   O      O  U       U SSSSSSSSS
+  |T|   O      O  U       U         S
+  |T|   O      O  U       U         S
+  |T|   OOOOOOOO   UUUUUUU  SSSSSSSSS
+
+""")
