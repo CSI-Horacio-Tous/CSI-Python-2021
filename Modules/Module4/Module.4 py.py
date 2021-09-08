5d825d45fdc2675911872ee09ac42122c257a4ac
CSI-Horacio-Tous <89258670+CSI-Horacio-Tous@users.noreply.github.com>
2021-09-08 09:23:06 -0400

    Create Module.4 py.py

---
+++ Modules/Module4/Module.4 py.py
@@ -0,0 +1,15 @@
+desayuno = 'Croissant con jamon queso y huevo.'
+Lunch= 'Arroz con habichuelas y carne.'
+dinner= 'Yes , salmon con pasta.'
+
+print("What did you have for Breakfast?")
+
+print (desayuno)
+
+print('What was your Lunch?')
+
+print (Lunch)
+
+print('Have you had dinner?')
+
+print (dinner)
