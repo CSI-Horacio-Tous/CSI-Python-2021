0126218c501d90e0ab3c08ff3306ed622f73898d
CSI-Horacio-Tous <89258670+CSI-Horacio-Tous@users.noreply.github.com>
2021-09-14 09:23:00 -0400

    Update Module.4 py.py

--- Modules/Module4/Module.4 py.py
+++ Modules/Module4/Module.4 py.py
@@ -1,15 +1,55 @@
-desayuno = 'Croissant con jamon queso y huevo.'
-Lunch= 'Arroz con habichuelas y carne.'
-dinner= 'Yes , salmon con pasta.'
+# desayuno = 'Croissant con jamon queso y huevo.'
+# Lunch= 'Arroz con habichuelas y carne.'
+# dinner= 'Yes , salmon con pasta.'
 
-print("What did you have for Breakfast?")
+# print("What did you have for Breakfast?")
 
-print (desayuno)
+# print (desayuno)
 
-print('What was your Lunch?')
+# print('What was your Lunch?')
 
-print (Lunch)
+# print (Lunch)
 
-print('Have you had dinner?')
+# print('Have you had dinner?')
 
-print (dinner)
+# print (dinner)
+
+
+
+
+# def greetStudent(name):
+#   print(f"Hello {name}!")
+
+# greetStudent('Horacio')
+
+
+# 
+# someNumber: int = 360
+# print(f'The Number is {someNumber}')
+
+# var:str = "regular string"
+# myNumber:float = 3.5
+
+# myFunctionalString = f"Combine an existing {var} with a number such as: {myNumber} or execute something like {round(3.4 * 1.1)}"
+
+# print(myFunctionalString)
+
+# print(f"Or use the print directly {myNumber}")
+
+# name:str = "Horacio Tous"
+# age:int = 15
+
+# intro = f"Hi my name is {name} and I am {age} years old."
+
+# print(intro)
+
+# s0 ="What is my length? Store it as a number in a variable."
+# ss0= len(s0)
+# print(ss0)
+
+s1 = "Hello Student! My name is Carlos Jose Cobián. I enjoy coding and video games!"
+ss1 = (s1[26:44])
+print(ss1)
+
+SS1 =ss1.upper()
+print(SS1)
