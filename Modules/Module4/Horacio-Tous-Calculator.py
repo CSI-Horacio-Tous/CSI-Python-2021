c978e8b630c60cc388c8aa610d605ac48d924df9
CSI-Horacio-Tous <89258670+CSI-Horacio-Tous@users.noreply.github.com>
2021-09-16 09:41:02 -0400

    Create Horacio-Tous-Calculator.py

---
+++ Modules/Module4/Horacio-Tous-Calculator.py
@@ -0,0 +1,19 @@
+# print('HELLO WORLD!!!')
+# print('What is your name')
+# name = input()
+# prin   ('it is good to meet to you', name)
+# print ('The lenght of your name is : ', len (name))
+# print ('How old are you')
+# age = input ()
+# print ('You kinda old , you will be ' + str(int(age) + 5) + ' in five years')
+
+# print('Hello this is Calcutron3000 , I will help you with the area of a rectangle')
+# print('The equation is A = width X height')
+# print('Please instert the values')
+# width = int(input('What is the width:'))
+# height= int(input('Whats the height:'))
+# print('The area of rectange is:', (width * height))
+
+
+def Area(width, height ):
+    print(f'Area of the rectangle is')
