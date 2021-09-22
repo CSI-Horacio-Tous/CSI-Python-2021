d6df09698f2cfc90fa6d433ec6d1f1e151e0e01a
CSI-Horacio-Tous <89258670+CSI-Horacio-Tous@users.noreply.github.com>
2021-09-22 09:30:50 -0400

    Create tiketera.py

---
+++ Modules/Module4/tiketera.py
@@ -0,0 +1,13 @@
+print ('Welcome to RKM & Ken-Y Concert')
+print ('The cost of each seat is as follow :Balcony Level = $50 , PLaza Level 1 = $75 , VIP Meet and Greet = $150')
+Balcony= int(input('How many tickets sold for Balcony Level='))
+Plaza= int(input('How many tickets sold for Plaza Level 1 ='))
+VIP= int(input('How many tickets sold for VIP Meet and Greet='))
+
+Balconyprice=(print('The cost for Balcony Level Tickets is: $', int(Balcony) * 50 ))
+Plazaprice=(print('The cost for Plaza Level 1 Tickets is: $', int(Plaza) * 75 ))
+Vipprice= (print('The cost for VIP Meet and Greet Tickets is: $', int(VIP) * 150 ))
+
+
+
+total =print int(Balconyprice) + (Plazaprice) + (Vipprice)) 
