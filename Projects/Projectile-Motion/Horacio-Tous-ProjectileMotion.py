79070e0ce9079dc479a3f852f6ae1cd5c2ccc106
CSI-Horacio-Tous <89258670+CSI-Horacio-Tous@users.noreply.github.com>
2021-11-05 09:12:37 -0400

    Update Horacio-Tous-ProjectileMotion.py

--- Projects/Projectile-Motion/Horacio-Tous-ProjectileMotion.py
+++ Projects/Projectile-Motion/Horacio-Tous-ProjectileMotion.py
@@ -9,16 +9,40 @@
 # gravity= 9.8
 
 import math
+from ExperimentalData import ExperimentalData
 
-# time_s = math.sqrt((2 * Buildingheight) / gravity) 
+def Projectilefunction(expermientalData: ExperimentalData):
 
-def Projectilefunction(gun:str , caliber:str , amunition:str , Velocity:int ,Buildingheight:int , Building:str , gravity:int )
+    time_s = math.sqrt((2 * experimentalData.Buildingheight) / experimentalData.gravity) 
+
+    distance_m =(experimentalDAta.Velocity * time_s)
+    #distance_m =(experimentalData[Velocity] * time_s)
 
 
-print(f'The gun selected for the experiment is {gun} and it uses {caliber} caliber with {ammunition} ammuniton.')
+print(f'The gun selected for the experiment is {expermientalData.gun} and it uses {expermientalData.caliber} caliber with {expermientalData.ammunition} ammuniton.')
 
-print(f'The gun with that ammunition has a projectile speed of {Velocity} ms')
+print(f'The gun with that ammunition has a projectile speed of {expermientalData.Velocity} ms')
 
-print(f'The experiment will take place in {Building} that is {Buildingheight} ft tall.')
+print(f'The experiment will take place in {expermientalData.Building} that is {expermientalData.Buildingheight} ft tall.')
 
-Projectilefunction('Glock 17','9 X 19mm' , 'PBP gzh' , 560 , 334 ,'Caribbean Sea Views' , 'Caribbean Sea Views'
+#convert your script parameters into a singles JSON Object
+
+experimentalData= {
+
+'gun': "Glock 17" ,
+'caliber' : ' 9 X 19mm ',
+'ammunition': 'PBP gzh', 
+'Velocity' : 560 ,
+'Building' : 'Caribbean Sea Views',
+'Buildingheight' : 334 ,
+'gravity': 9.8
+
+
+
+
+
+}
+
+experimentalData = ExperimentalData('Glock 17','9 X 19mm' , 'PBP gzh' , 560 , 334 ,'Caribbean Sea Views' , 'Caribbean Sea Views'
+
+Projectilefunction
