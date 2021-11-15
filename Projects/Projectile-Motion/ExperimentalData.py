397eff68879ffd34a2ffb56a7bed663d692b8fbc
CSI-Horacio-Tous <89258670+CSI-Horacio-Tous@users.noreply.github.com>
2021-11-15 08:30:41 -0400

    Update ExperimentalData.py

--- Projects/Projectile-Motion/ExperimentalData.py
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -1,5 +1,5 @@
 class ExperimentalData:
-    def __init__(self ,gun:str , caliber:str , amunition:str , Velocity:int ,Buildingheight:int , Building:str , gravity:int  ):
+    def __init__(self ,gun:str, caliber:str, amunition:str , Velocity:int, Buildingheight:int, Building:str, gravity:float):
         self.gun = gun
         self.caliber= caliber
         self.amunition= amunition
