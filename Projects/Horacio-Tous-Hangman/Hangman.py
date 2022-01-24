8d2b11a95d3f1dc74701ef593a99bf65491b06fe
CSI-Horacio-Tous <89258670+CSI-Horacio-Tous@users.noreply.github.com>
2022-01-24 09:23:18 -0400

    Update Hangman.py

--- Projects/Horacio-Tous-Hangman/Hangman.py
+++ Projects/Horacio-Tous-Hangman/Hangman.py
@@ -1,9 +1,36 @@
 import random
 word_list = ["Bayamon", "Aguadilla", "Rincon", "Patillas", "Arecibo", "Loiza", "Aguada",
 "Cayey", "Jayuya", "Isabela" ]
+def get_word(word_list):
+    word=random.choice(word_list)
+    return word.upper()
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters= []
+    guessed_words = []
+    tries = 6
+    print('A Jugar Hangman')
+    print('Tema = Pueblos de Puerto Rico')
+    print(display_hangman(tries))
+    print(word_completion)
+    [print("\n")]
+    while not guessed and tries > 0:
+        guess = input("Guess a letter or word").upper()
+        if len(guess) == 1 and guess.isalpha():
+            print("You already tried" , guess, "!")
+        elif guess not in word:
+            print("Isn't in the word : (")
+            tries -= 1
+            guessed_letters.append(guess)
+
+
+
+
 def display_hangman(tries):
  stages = [ """
- --------
+--------
  | |
  | O
  |/|\\
