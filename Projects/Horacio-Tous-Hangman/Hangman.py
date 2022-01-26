c64ee1ae381718c78869694ea1fdb46a1e7ac4d5
CSI-Horacio-Tous <89258670+CSI-Horacio-Tous@users.noreply.github.com>
2022-01-26 09:24:52 -0400

    Update Hangman.py

--- Projects/Horacio-Tous-Hangman/Hangman.py
+++ Projects/Horacio-Tous-Hangman/Hangman.py
@@ -1,3 +1,4 @@
+from curses.ascii import isalpha
 import random
 word_list = ["Bayamon", "Aguadilla", "Rincon", "Patillas", "Arecibo", "Loiza", "Aguada",
 "Cayey", "Jayuya", "Isabela" ]
@@ -19,11 +20,43 @@ def play(word):
     while not guessed and tries > 0:
         guess = input("Guess a letter or word").upper()
         if len(guess) == 1 and guess.isalpha():
-            print("You already tried" , guess, "!")
-        elif guess not in word:
-            print("Isn't in the word : (")
-            tries -= 1
-            guessed_letters.append(guess)
+            if guess in guessed_letters:
+                print("You already tried" , guess, "!")
+            elif guess not in word:
+                print( guess, "Isn't in the word : (")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Nice one", guess 'is in the word!')
+                guessed_letters .append(guess)
+                word_as_list= list(word_completion)
+                indices = [i for i , letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index]=guess 
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in  guessed_words:
+                print("You already tried" , guess , "!")
+            elif guess != word:
+                print(guess, "isn't the word")
+                tries -=1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("invalid input")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    if guessed:
+        print("Good job , you guessed the word !!! :)")
+    else:
+        print("I'm sorry , but you ran out of tries. The word was" + word + ".Try again")
+            
+
 
 
 
