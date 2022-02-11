5a021595fd469623a4f9ce4e07d17a384bcc99cf
CSI-Horacio-Tous <89258670+CSI-Horacio-Tous@users.noreply.github.com>
2022-02-11 09:21:48 -0400

    Update snake.py

--- Projects/snake.py
+++ Projects/snake.py
@@ -1,4 +1,4 @@
-from dis import dis
+
 import pygame
 pygame.init() # inicia todos los médules de PuBame
 
@@ -8,14 +8,42 @@ red = (213, 50, 80) #game over
 green = (0, 255, 0) #food
 blue = (50, 153, 213) #snake
 
-dis-pygame.display.set_mode((400,300))
-pygame.display.update() #actualiza los cambios que ocurtan en la pantalla
-pygame.display.set_caption("Snake game by JD") # ARaneserá el nombre del jusse
+dis=pygame.display.set_mode((400,300))
+pygame.display.set_caption("Snake game by HT") # ARaneserá el nombre del jusse
 game_over=False
+
+x1 = 150
+y1 = 150
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
+
 while not game_over:
     for event in pygame.event.get():
         if event.type== pygame.QUIT: #cierra la pantalla al pinchar la (x))
             game_over=True
-           
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                x1_change = -10
+                y1_change = 0
+            elif  event.key == pygame.K_d:
+                x1_change = 10
+                y1_change = 0
+            elif  event.key == pygame.K_w:
+                x1_change = 0
+                y1_change = -10
+            elif event.key == pygame.K_s:
+                x1_change = 0
+                y1_change = 10
+    x1 += x1_change
+    y1 += y1_change
+        
+    dis.fill(black)
+    pygame.draw.rect(dis, blue, [x1 ,y1 ,10 ,10])#draw snake
+    pygame.display.update() #actualiza los cambios que ocurtan en la pantalla       
+
+    clock.tick(15) #velocidad
 pygame.quit() #termina todo
 quit()
